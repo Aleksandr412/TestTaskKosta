b545e13545d01ebd2952ab7e1669e3a1adf6b785
Alexandrs412 <alexandr.s412@gmail.com>
2018-10-19 03:21:49 +0300

    first commit

---
+++ TestTaskKosta2/TestTaskKosta2/Form1.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace TestTaskKosta2
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnShowOrgStruct = new System.Windows.Forms.Button();
+            this.ShowDeptEmpl = new System.Windows.Forms.Button();
+            this.cmbDep = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.tbAge = new System.Windows.Forms.TextBox();
+            this.tbPosition = new System.Windows.Forms.TextBox();
+            this.tbDOB = new System.Windows.Forms.TextBox();
+            this.tbPatronymic = new System.Windows.Forms.TextBox();
+            this.tbSurName = new System.Windows.Forms.TextBox();
+            this.tbFirstName = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.Location = new System.Drawing.Point(14, 201);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(629, 307);
+            this.dataGridView1.TabIndex = 5;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // btnShowOrgStruct
+            // 
+            this.btnShowOrgStruct.Location = new System.Drawing.Point(3, 3);
+            this.btnShowOrgStruct.Name = "btnShowOrgStruct";
+            this.btnShowOrgStruct.Size = new System.Drawing.Size(226, 45);
+            this.btnShowOrgStruct.TabIndex = 1;
+            this.btnShowOrgStruct.Text = "Структура предприятия";
+            this.btnShowOrgStruct.UseVisualStyleBackColor = true;
+            this.btnShowOrgStruct.Click += new System.EventHandler(this.btnShowOrgStruct_Click);
+            // 
+            // ShowDeptEmpl
+            // 
+            this.ShowDeptEmpl.Location = new System.Drawing.Point(3, 104);
+            this.ShowDeptEmpl.Name = "ShowDeptEmpl";
+            this.ShowDeptEmpl.Size = new System.Drawing.Size(226, 40);
+            this.ShowDeptEmpl.TabIndex = 2;
+            this.ShowDeptEmpl.Text = "Показать сотрудников отдела";
+            this.ShowDeptEmpl.UseVisualStyleBackColor = true;
+            this.ShowDeptEmpl.Click += new System.EventHandler(this.ShowDeptEmpl_Click);
+            // 
+            // cmbDep
+            // 
+            this.cmbDep.FormattingEnabled = true;
+            this.cmbDep.Location = new System.Drawing.Point(3, 77);
+            this.cmbDep.Name = "cmbDep";
+            this.cmbDep.Size = new System.Drawing.Size(226, 21);
+            this.cmbDep.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Выбор отдела:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(67, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Имя:";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnShowOrgStruct);
+            this.panel1.Controls.Add(this.ShowDeptEmpl);
+            this.panel1.Controls.Add(this.cmbDep);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(15, 28);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(234, 167);
+            this.panel1.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.tbAge);
+            this.panel2.Controls.Add(this.tbPosition);
+            this.panel2.Controls.Add(this.tbDOB);
+            this.panel2.Controls.Add(this.tbPatronymic);
+            this.panel2.Controls.Add(this.tbSurName);
+            this.panel2.Controls.Add(this.tbFirstName);
+            this.panel2.Controls.Add(this.label7);
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(256, 28);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(387, 167);
+            this.panel2.TabIndex = 6;
+            // 
+            // tbAge
+            // 
+            this.tbAge.Location = new System.Drawing.Point(105, 133);
+            this.tbAge.Name = "tbAge";
+            this.tbAge.ReadOnly = true;
+            this.tbAge.Size = new System.Drawing.Size(187, 20);
+            this.tbAge.TabIndex = 1;
+            // 
+            // tbPosition
+            // 
+            this.tbPosition.Location = new System.Drawing.Point(105, 107);
+            this.tbPosition.Name = "tbPosition";
+            this.tbPosition.ReadOnly = true;
+            this.tbPosition.Size = new System.Drawing.Size(187, 20);
+            this.tbPosition.TabIndex = 1;
+            // 
+            // tbDOB
+            // 
+            this.tbDOB.Location = new System.Drawing.Point(105, 81);
+            this.tbDOB.Name = "tbDOB";
+            this.tbDOB.ReadOnly = true;
+            this.tbDOB.Size = new System.Drawing.Size(187, 20);
+            this.tbDOB.TabIndex = 1;
+            // 
+            // tbPatronymic
+            // 
+            this.tbPatronymic.Location = new System.Drawing.Point(105, 55);
+            this.tbPatronymic.Name = "tbPatronymic";
+            this.tbPatronymic.ReadOnly = true;
+            this.tbPatronymic.Size = new System.Drawing.Size(187, 20);
+            this.tbPatronymic.TabIndex = 1;
+            // 
+            // tbSurName
+            // 
+            this.tbSurName.Location = new System.Drawing.Point(105, 6);
+            this.tbSurName.Name = "tbSurName";
+            this.tbSurName.ReadOnly = true;
+            this.tbSurName.Size = new System.Drawing.Size(187, 20);
+            this.tbSurName.TabIndex = 1;
+            // 
+            // tbFirstName
+            // 
+            this.tbFirstName.Location = new System.Drawing.Point(105, 29);
+            this.tbFirstName.Name = "tbFirstName";
+            this.tbFirstName.ReadOnly = true;
+            this.tbFirstName.Size = new System.Drawing.Size(187, 20);
+            this.tbFirstName.TabIndex = 1;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(33, 110);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(68, 13);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Должность:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(49, 136);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Возраст:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 84);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(89, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Дата рождения:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(44, 58);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Отчество:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Фамилия:";
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(655, 520);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form";
+            this.Text = "TestTask";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnShowOrgStruct;
+        private System.Windows.Forms.Button ShowDeptEmpl;
+        private System.Windows.Forms.ComboBox cmbDep;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox tbFirstName;
+        private System.Windows.Forms.TextBox tbAge;
+        private System.Windows.Forms.TextBox tbPosition;
+        private System.Windows.Forms.TextBox tbDOB;
+        private System.Windows.Forms.TextBox tbPatronymic;
+        private System.Windows.Forms.TextBox tbSurName;
+    }
+}
+
---
+++ TestTaskKosta2/TestTaskKosta2/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace TestTaskKosta2
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        SqlConnection connection = new SqlConnection(@"Data Source = .\SQLEXPRESS; Database=TestDB; Trusted_Connection=True;");
+
+        public Form()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            connection.Open();
+            SqlCommand command = connection.CreateCommand();
+            command.CommandType = CommandType.Text;
+            command.CommandText = "SELECT DISTINCT Name FROM Department";
+            command.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(command);
+            da.Fill(dt);
+            foreach (DataRow dr in dt.Rows)
+            {
+                cmbDep.Items.Add(dr["Name"].ToString());
+            }
+            connection.Close();
+        }
+
+        //Структура организации
+        private void btnShowOrgStruct_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            SqlCommand command = connection.CreateCommand();
+            command.CommandType = CommandType.Text;
+            command.CommandText = "SELECT DISTINCT Name, Position FROM Department, Employee";
+            command.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(command);
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            connection.Close();
+        }
+
+        //Сотрудник выбранного отдела
+        private void ShowDeptEmpl_Click(object sender, EventArgs e)
+        {
+            if (cmbDep.SelectedItem != null)
+            {
+                string some_str = cmbDep.SelectedItem.ToString();
+
+                connection.Open();
+                SqlCommand command = connection.CreateCommand();
+                command.CommandType = CommandType.Text;
+                command.CommandText = "SELECT FirstName, Surname, Patronymic, Position, DateOfBirth FROM Employee WHERE DepartmentID IN (SELECT ID FROM Department WHERE Name = '" + cmbDep.SelectedItem.ToString() + "')";
+                command.ExecuteNonQuery();
+                DataTable dt = new DataTable();
+                SqlDataAdapter da = new SqlDataAdapter(command);
+                da.Fill(dt);
+                dataGridView1.DataSource = dt;
+                connection.Close();
+            }
+            else
+            {
+                MessageBox.Show("Сначала выберите отдел");
+            }
+        }
+
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                try
+                {
+                    DataGridViewRow row = this.dataGridView1.Rows[e.RowIndex];
+
+                    tbFirstName.Text = row.Cells["FirstName"].Value.ToString();
+                    tbSurName.Text = row.Cells["SurName"].Value.ToString();
+                    tbPatronymic.Text = row.Cells["Patronymic"].Value.ToString();
+                    tbPosition.Text = row.Cells["Position"].Value.ToString();
+                    tbDOB.Text = row.Cells["DateOfBirth"].Value.ToString();
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+        }
+
+    }
+}
